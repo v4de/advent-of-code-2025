5009aa46c326f902b2be76d58117669a07065dd9
v4de <934222+v4de@users.noreply.github.com>
2026-01-02 16:54:56 -0500

    update tsconfig and make funciton async

--- 2/src/app.ts
+++ 2/src/app.ts
@@ -10,25 +10,54 @@ import { createInterface } from 'readline';
 // 123123 is 123 twice
 // Find all the invalid IDs and add them together
 
-function findInvalidIds(filePath: string) {
-  const fileStream = createReadStream(filePath);
+async function findInvalidIds(filePath: string): Promise<number> {
+  let sum = 0;
 
+  const fileStream = createReadStream(filePath);
   const rl = createInterface({
     input: fileStream,
     crlfDelay: Infinity
   });
 
-  rl.on('line', (line: string) => {
+  for await (const line of rl) {
+    const idRanges = line.split(',').map(id => id.trim());
 
-  });
+    for await (const idRange of idRanges) {
+      const [startStr, endStr] = idRange.split('-').map(part => part.trim());
+      const start = parseInt(startStr, 10);
+      const end = parseInt(endStr, 10);
 
-  return new Promise<void>((resolve) => {
-    rl.on('close', () => {
-      resolve();
-    });
-  });
+      const ids = range(start, end);
+      for await (const id of ids) {
+        if (isInvalidId(id.toString())) {
+          sum += id;
+          console.log(`Invalid ID found: ${id}`);
+        }
+      }
+    }
+  };
+  return sum
+}
+
+function isInvalidId(id: string) {
+  const len = id.length;
+  if (len % 2 !== 0) {
+    return false;
+  }
+  const half = len / 2;
+  const firstHalf = id.substring(0, half);
+  const secondHalf = id.substring(half);
+  return firstHalf === secondHalf;
+}
+
+function range(start: number, end: number) {
+  const result = [];
+  for (let i = start; i <= end; i++) {
+    result.push(i);
+  }
+  return result;
 }
 
 // Example usage
 const filePath = 'src/day2.txt';
-findInvalidIds(filePath).catch(console.error);
+console.log("Sum of invalid IDs:", await findInvalidIds(filePath).catch(console.error));
